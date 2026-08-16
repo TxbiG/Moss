4edfc8437ef621167a2d27dede24544ba22cd946
Toby <126118957+TxbiG@users.noreply.github.com>
2026-08-16 16:57:00 +0100

    Update Moss.h

--- include/Moss/Moss.h
+++ include/Moss/Moss.h
@@ -46,5 +46,5 @@
 //#include <Moss/Moss_GUI.h>                  // Contains
 //#include <Moss/Moss_Physics.h>              // Contains all the physics side for 2D & 3D
 //#include <Moss/Moss_Network.h>              // Contains all network libraries needed by enet6.
-#include <Moss/Moss_XR.h>                   // Contains
+//#include <Moss/Moss_XR.h>                   // Contains
 //#include <Moss/Moss_Navigation.h>           // Contains
